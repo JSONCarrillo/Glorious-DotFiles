bab7b28ff4c65597e12c613d23a26dd0b3c8be05
Jason <rockintech@proton.me>
2024-10-31 14:43:28 -0600

    update nvim and .scripts

---
+++ .scripts/dotsback.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+dotfile_directory=~/Dev/Glorious-DotFiles
+
+cp -r ~/.config/awesome $dotfile_directory
+cp -r ~/.config/picom $dotfile_directory
+cp -r ~/.config/kitty $dotfile_directory
+cp -r ~/.config/nvim $dotfile_directory
+cp -r ~/.scripts $dotfile_directory
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,5 @@
+-- plugins/telescope.lua:
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+-- or                              , branch = '0.1.x',
+    }
